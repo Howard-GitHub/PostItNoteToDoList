b63eb5c9e4d55932fc0da3588d29a445c5e4623d
hlee123456 <hleesays@gmail.com>
2024-06-03 21:02:53 -0700

    Feature: Add button bar for each post it note section

---
+++ to_do_list/src/components/PostItNoteSection/SectionButtonBar/SectionButtonBar.jsx
@@ -0,0 +1,13 @@
+import './SectionButtonBar.css';
+
+const SectionButtonBar = () => {
+    return ( 
+        <div className="section-button-bar-container">
+            <div className="section-button-bar">
+                
+            </div>
+        </div>
+     );
+}
+ 
+export default SectionButtonBar;
