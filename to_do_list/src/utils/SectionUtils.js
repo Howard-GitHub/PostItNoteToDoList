cc6f150c90c4d90b17aa9be753af1a340b5a852d
hlee123456 <hleesays@gmail.com>
2024-06-13 18:55:06 -0700

    Refactor: Change identifier names to be more general

--- to_do_list/src/utils/SectionUtils.js
+++ to_do_list/src/utils/SectionUtils.js
@@ -1,15 +1,15 @@
 // Changes the height of the textarea everytime a new line is added
-export const handleChangeTextareaHeight = (titleRef) => {
-    if (titleRef.current) {
-        titleRef.current.style.height = 'auto';
-        titleRef.current.style.height = `${titleRef.current.scrollHeight}px`;
+export const handleChangeTextareaHeight = (textareaRef) => {
+    if (textareaRef.current) {
+        textareaRef.current.style.height = 'auto';
+        textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
     }
 }
 
 // Tracks and saves user input for the title of a post it note section
-export const handleOnChangeKeyboardInput = (event, titleRef, setTitle) => {
-    setTitle(event.target.value);
-    handleChangeTextareaHeight(titleRef);
+export const handleOnChangeKeyboardInput = (event, textareaRef, setText) => {
+    setText(event.target.value);
+    handleChangeTextareaHeight(textareaRef);
 }
 
 // Identifies which post it note section textarea has been selected to be edited and prevents other post it note section textareas from becoming selected
