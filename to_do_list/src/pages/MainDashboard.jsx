9223071334732c96f1bc146e36771b37cd354713
hlee123456 <hleesays@gmail.com>
2024-07-12 21:39:42 -0700

    Feature: Add feature to resize flex container based on the size of the window tab
    The div named "main-dashboard" is the flex container that resizes based on the window tab. This flex container is resized so that the container stays symmetrical and centers on the screen when the user resizes the window tab.

--- to_do_list/src/pages/MainDashboard.jsx
+++ to_do_list/src/pages/MainDashboard.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useEffect, useState, useRef} from 'react';
 import PostItNoteSection from '../components/PostItNoteSection/PostItNoteSection';
 import ItemsButtonBar from '../components/ItemsButtonBar/ItemsButtonBar';
 import ExitDeleteItemModeButton from '../components/ExitDeleteItemModeButton/ExitDeleteItemModeButton';
@@ -10,17 +10,35 @@ const MainDashboard = () => {
 
     // Contains the post it note section. A post it note section contains multiple post it notes or "a section of post it notes" and is supposed to
     // contain one or more individual post it notes
+    const mainDashboardRef = useRef();
     const [arrayOfSections, setArrayOfSections] = useState([]);
     const [isInDeleteMode, setIsInDeleteMode] = useState(false);
     const [oneSectionIsEntered, setOneSectionIsEntered] = useState(false);
     const [selectedSection, setSelectedSection] = useState(null);
     const {handleClickAddItem, handleClickDeleteItem} = useLocalStoredArray("mainDashboard", arrayOfSections, setArrayOfSections);
 
+    useEffect(() => {
+        // Resizes flex container so the group of post-it-notes stay centered
+        const handleResizeFlexContainer = () => {
+            // Width of post it note plus its padding
+            const postItNoteTotalWidth = 410;
+            const multiplyer = Math.floor(window.innerWidth / postItNoteTotalWidth);
+            if (mainDashboardRef.current) {
+                mainDashboardRef.current.style.width = `${postItNoteTotalWidth * multiplyer}px`;
+            }
+        }
+        window.addEventListener("resize", handleResizeFlexContainer);
+        return () => {
+            window.removeEventListener("resize", handleResizeFlexContainer);
+        }
+    }, [])
 
     return ( 
             <div className="main-dashboard-container">
-                <div className="main-dashboard">
-
+                <div 
+                    className="main-dashboard"
+                    ref={mainDashboardRef}
+                >
                     {arrayOfSections.map((singlePostItNoteSection) => (
                         <PostItNoteSection 
                             key={singlePostItNoteSection.id}
